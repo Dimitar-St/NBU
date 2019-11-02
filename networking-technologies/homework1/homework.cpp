5ff9bd832ee79de1e9fa7c74493844818a9c553f
dimitar <stoyanov.dimi.tri@gmail.com>
2019-11-02 20:39:50 +0200

    Added the first homework

---
+++ networking-technologies/homework1/homework.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{	
+	cout << "Enter four strings: " << endl;	
+
+	string text_input1 = "";
+	cin >> text_input1;
+
+	string text_input2 = "";
+	cin >> text_input2;
+
+	string text_input3 = "";
+	cin >> text_input3;
+
+	string text_input4 = "";
+	cin >> text_input4;
+
+	string text[] = { text_input1, text_input2, text_input3, text_input4 };
+
+		
+	for(int i = 0; i <= 3; i += 1)
+ 	{
+		string current_text = text[i];
+	
+		for(int j = 0; j <= 3; j++)
+		{
+	
+			if(current_text < text[j]) 
+			{	
+				string temp_text = text[i];
+				text[i]  = text[j];
+				text[j] = temp_text;
+			}
+		}
+	}
+
+	cout << "Ascending order: " << endl;
+
+	for(int i = 0; i <= 3; i++) 
+	{	
+		cout << text[i] << endl;
+	}
+
+	return 0;
+
+
+}
